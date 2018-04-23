21a5c1170af6a3cbd87f0de99b26edcc52117ad6
rbarnada <ryan.barnada@gmail.com>
2018-04-23 10:11:03 -0400

    Update Run model
    Model
    - Edited time format to only display in HH:MM:SS

--- app/models/run.rb
+++ app/models/run.rb
@@ -3,5 +3,8 @@
 class Run < ApplicationRecord
   # include Authentication
   belongs_to :user
+  attribute :time do
+    object.time.strftime '%T'
+  end
   validates :distance, :time, :user, presence: true
 end
